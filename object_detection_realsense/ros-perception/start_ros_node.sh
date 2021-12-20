317dcb25956b1f1653596b0bfd9e5612eafa1f8a
HichDh <hichem.dhouib.tn@gmail.com>
2021-12-20 01:02:02 +0100

    removed old files and folder from new branch

--- object_detection_realsense/ros-perception/start_ros_node.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source /home/catkin_ws/devel/setup.bash
-export ROS_MASTER_URI=http://192.168.0.7:11311
-export ROS_IP=192.168.0.7
-export ROS_HOSTNAME=192.168.0.7
-./mmdetection_ros_node.py
